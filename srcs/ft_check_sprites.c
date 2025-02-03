9a4b0ea20864b23ae05f5bbbb3a494181d10f1be
Mohamed Ait Alla <mait-all@c4r7p9.1337.ma>
2025-02-03 12:17:08 +0100

    Update code: fix an when the map file doesn't end with .ber extention

--- srcs/ft_check_sprites.c
+++ srcs/ft_check_sprites.c
@@ -6,7 +6,7 @@
 /*   By: mait-all <mait-all@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 21:07:30 by mait-all          #+#    #+#             */
-/*   Updated: 2025/02/02 17:59:20 by mait-all         ###   ########.fr       */
+/*   Updated: 2025/02/03 12:11:42 by mait-all         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,11 +42,9 @@ void	check_file_extention(char *file)
 
 	file_set = ft_memchr(file, '.', ft_strlen(file));
 	file_set_h = ft_memchr(file, '/', ft_strlen(file)) + 1;
-	if (!file_set || !file_set_h)
-		return ;
-	if ((file_set_h[0] == '.' && file_set_h[1] == 'b'
+	if (!file_set || !file_set_h || (file_set_h[0] == '.' && file_set_h[1] == 'b'
 			&& file_set_h[2] == 'e' && file_set_h[3] == 'r')
-		|| (file_set[1] != 'b' || file_set[2] != 'e' || file_set[3] != 'r'))
+		|| (file_set[1] != 'b' || file_set[2] != 'e' || file_set[3] != 'r') )
 	{
 		ft_printf(RED "Error\n: Invalid map file extention!\n" RESET);
 		exit(1);
